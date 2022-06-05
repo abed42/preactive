fc4cdccd4dd28c182fbcbb4a6ba44a8c98dac73c
Abed <abedhamami42@gmail.com>
2022-06-05 13:13:31 +0300

    added react-router

--- src/App.js
+++ src/App.js
@@ -1,17 +1,60 @@
+import {
+  BrowserRouter,
+  Route,
+  Routes,
+  Link
+} from "react-router-dom";
+
+
 import './App.css';
+
 import TaskList from './components/TaskList';
+import AvoidTaskList from './components/AvoidTaskList';
+
+
 
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>p:reac:tive</h1> 
-      </header>
+    <BrowserRouter>
+    <>
 
-    <TaskList />
-    </div>
+      <div className="App">
+        <header className="App-header">
+          <h1>p:reac:tive</h1> 
+          <ul>
+            <li>
+              <Link to="/do">Tasks to do</Link>
+            </li>
+            <li>
+              <Link to="/avoid">Tasks to avoid</Link>
+            </li>
+          </ul>
+        </header>
+         
+        <Routes>
+          <Route path="/do" element={<TaskList />} />         
+          <Route path="/avoid" element={<AvoidTaskList  />} />
+        </Routes>
+      </div>
+    </>
+    </BrowserRouter>
   );
 }
 
 export default App;
+
+
+
+
+  {/* <nav>
+          <ul>
+            <li>
+              <Link to="/do">Tasks to do</Link>
+            </li>
+            <li>
+              <Link to="/avoid">Tasks to avoid</Link>
+            </li>
+          </ul>
+        </nav> */}
+       
--- src/components/AvoidTaskList.js
+++ src/components/AvoidTaskList.js
@@ -2,16 +2,16 @@ import { useState } from "react";
 import { v4 as uuidv4 } from 'uuid';
 import NewTaskForm from "./NewTaskForm";
 
-const TaskList = () => {
+const AvoidTaskList = () => {
 
     fetch()
 
 const [tasks, setTasks] =  useState([
-    {title: 'homework', id: 1}, 
-    {title: 'workout', id: 2},
-    {title: 'mediteta for 5 mins', id: 3},
-    {title: 'read for 30 mins', id: 4},
-    {title: 'issue an invoice', id: 5}
+    {title: 'endless scrolling', id: 1}, 
+    {title: 'drugs', id: 2},
+    {title: 'watching the news ', id: 3},
+    {title: 'social media', id: 4},
+    {title: 'junk food', id: 5}
 ]);
 
 const addTask = (title) => setTasks([...tasks, {title, id: uuidv4()}]);
@@ -21,11 +21,11 @@ const removeTask = (id) => setTasks(tasks.filter(task => task.id !== id ));
     return (  
         <div>
             <ul>
-            {tasks.length <= 0 &&
-                <h2>
-                nothing to do ! you've done everything (or nothing).
-                </h2>
-            }
+              {tasks.length <= 0 &&
+                    <h2>
+                    nothing to do ! you've done everything (or nothing).
+                    </h2>
+                }
                 {tasks.map( task => {
                 return (  <li key={task.id} >  {task.title} <span onClick={()=> removeTask(task.id)} style={{color :'red', marginLeft: '20px'}}>x</span> </li>  )
                 })}
@@ -36,4 +36,4 @@ const removeTask = (id) => setTasks(tasks.filter(task => task.id !== id ));
     );
 }
 
-export default TaskList;
+export default AvoidTaskList;
